8b48dab0d01ba955cecc96f844fa64fc7a4402d2
chesedo <pieter@chesedo.me>
2024-03-06 22:26:15 +0000

    refactor: library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use converter_test::convert;
 use std::io::{self, Read};
 
 fn main() {
@@ -8,49 +9,4 @@ fn main() {
     let json = convert(&buffer);
 
     println!("{json}");
-}
-
-fn convert(input: &str) -> String {
-    let value: serde_json::Value = quick_xml::de::from_str(&input).unwrap();
-
-    serde_json::to_string_pretty(&value).unwrap()
-}
-
-#[cfg(test)]
-mod tests {
-    use pretty_assertions::assert_eq;
-
-    use crate::convert;
-
-    #[test]
-    fn test() {
-        let actual = convert(
-            r#"<?xml version="1.0" encoding="UTF-8"?>
-        <note>
-          <to>Dove</to>
-          <from><name>Jane</name></from>
-          <heading>Reminder</heading>
-          <body>Learn Rust this weekend!</body>
-        </note>"#,
-        );
-
-        let expected = r#"{
-  "body": {
-    "$text": "Learn Rust this weekend!"
-  },
-  "from": {
-    "name": {
-      "$text": "Jane"
-    }
-  },
-  "heading": {
-    "$text": "Reminder"
-  },
-  "to": {
-    "$text": "Dove"
-  }
-}"#;
-
-        assert_eq!(actual, expected);
-    }
 }
